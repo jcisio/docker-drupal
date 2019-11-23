5f0f7ce8bb9df673207e55afec5939bfef94c2bc
Hai-Nam Nguyen <hainam@jcisio.com>
2019-11-23 09:48:02 +0100

    Fix DockerGo alias

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,19 +1,12 @@
-# Execute a command in the corresponding directory.
-if [[ -n "${BASH_SOURCE[0]}" ]]; then
-  CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-else
-  CURRENTDIR="$(dirname "$(which "$0")")"
-fi
-CURRENTDIR="$(dirname "$CURRENTDIR")"
-export DOCKER_PATH_SOURCE="$CURRENTDIR"
+#!/usr/bin/env bash
 
 DockerGo() {
-  source $DOCKER_PATH_SOURCE/.env
   docker_dest="/var/www"
   if [[ "$@" != "" ]]; then
     args=$@
   else
     args=bash
   fi
-  docker exec -it dockerdrupal_php_1 bash -c "cd ${PWD/#$DOCKER_PATH_WEB/$docker_dest}; exec $args"
+  # @todo detect PHP version.
+  docker exec -it drupal_php71 bash -c "cd ${PWD/#$DOCKER_PATH_WEB/$docker_dest}; exec $args"
 }
