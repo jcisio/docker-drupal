b481f7d343027fb345172d97199d2235ecf32a55
Hai-Nam Nguyen <hainam@jcisio.com>
2019-11-29 09:29:06 +0100

    Add a Drupal auto-config helper file

---
+++ php/drupal-settings.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Auto configuration for Drupal.
+ *
+ * Just put a single line in your settings.php:
+ *
+ *     $databases['default']['default']['database'] = 'myproject';
+ *     require '/etc/drupal-settings.php';
+ *
+ * Then it will fill all the connection details. It will also create the database if not exists yet.
+ */
+if (isset($databases['default']['default']['database']) && !isset($databases['default']['default']['host'])) {
+  $_db = [
+    'host' => 'mariadb',
+    'driver' => 'mysql',
+    'username' => 'root',
+    'password' => 'password',
+  ];
+  $name = $databases['default']['default']['database'];
+  $conn = new PDO("{$_db['driver']}:host={$_db['host']}", $_db['username'], $_db['password']);
+  if ($conn->prepare('CREATE SCHEMA IF NOT EXISTS ' . $name)->execute()) {
+    $databases['default']['default'] += $_db;
+  }
+}
