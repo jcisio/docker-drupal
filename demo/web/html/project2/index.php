9085127c679706a1e23ebe29c6b0e60fe93dee5b
Hai-Nam Nguyen <hainam@jcisio.com>
2019-12-09 11:19:13 +0100

    Add xhprof viewer

--- demo/web/html/project2/index.php
+++ demo/web/html/project2/index.php
@@ -3,8 +3,27 @@
 <ul>
   <li><a href="http://pma.docker.localhost/">PhpMyAdmin</a></li>
   <li><a href="http://mailhog.docker.localhost/">mailhog</a></li>
+  <li><a href="http://xhprof.docker.localhost/">Xhprof viewer</a> (there are traces of this page)</li>
   <li><a href="http://portainer.docker.localhost/">Portainer</a></li>
   <li><a href="http://docker.localhost:8080/">Traefik</a></li>
 </ul>
 
-<?php phpinfo();
+<?php
+
+// Start profiling.
+if (extension_loaded('tideways_xhprof')) {
+  tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_CPU);
+}
+
+phpinfo();
+
+for ($i = 0; $i < 100000; $i++) {
+  $temp = random_int(0, 999999);
+}
+
+// Store profile.
+if (extension_loaded('tideways_xhprof')) {
+  $xhprof_out = '/mnt/files/private/xhprof';
+  file_exists($xhprof_out) || mkdir($xhprof_out);
+  file_put_contents(sprintf('%s/%s.%s.xhprof', $xhprof_out, uniqid(), 'web'), serialize(tideways_xhprof_disable()));
+}
