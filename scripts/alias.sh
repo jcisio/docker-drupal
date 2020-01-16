d7f805c3297ae520258f4c7bf0c5323d8021ca40
Hai-Nam Nguyen <hainam@jcisio.com>
2020-01-16 16:47:47 +0100

    Auto choose PHP container

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -8,5 +8,17 @@ DockerGo() {
     args=bash
   fi
   # @todo detect PHP version.
-  docker exec -it -e SSH_AUTH_SOCK=/ssh-agent drupal_php71 bash -c "cd ${PWD/#$DOCKER_PATH_WEB/$docker_dest}; exec $args"
+  RELATIVE_PATH=${PWD/#$DOCKER_PATH_WEB\/html\//}
+  VHOST_FILE="$DOCKER_PATH_WEB/conf/apache/vhosts.conf"
+  PHP_VERSION=$(grep $RELATIVE_PATH $VHOST_FILE | awk '{print $NF}')
+  if [[ $PHP_VERSION = "5.6" ]]; then
+    CONTAINER=php56
+  elif [[ $PHP_VERSION = "7.2" ]]; then
+    CONTAINER=php72
+  elif [[ $PHP_VERSION = "7.3" ]]; then
+    CONTAINER=php73
+  else
+    CONTAINER=php71
+  fi
+  docker exec -it -e SSH_AUTH_SOCK=/ssh-agent drupal_$CONTAINER bash -c "cd ${PWD/#$DOCKER_PATH_WEB/$docker_dest}; exec $args"
 }
