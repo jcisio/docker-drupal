8773ac41a8f5bbafd5ace2ebab65c160cb381860
Hai-Nam Nguyen <hainam@jcisio.com>
2020-01-17 11:13:19 +0100

    Improve PHP version matching logic

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -11,14 +11,10 @@ DockerGo() {
   RELATIVE_PATH=${PWD/#$DOCKER_PATH_WEB\/html\//}
   VHOST_FILE="$DOCKER_PATH_WEB/conf/apache/vhosts.conf"
   PHP_VERSION=$(grep $RELATIVE_PATH $VHOST_FILE | awk '{print $NF}')
-  if [[ $PHP_VERSION = "5.6" ]]; then
-    CONTAINER=php56
-  elif [[ $PHP_VERSION = "7.2" ]]; then
-    CONTAINER=php72
-  elif [[ $PHP_VERSION = "7.3" ]]; then
-    CONTAINER=php73
+  if [[ $PHP_VERSION =~ ^[57].[0-9]$ ]]; then
+    CONTAINER=php${PHP_VERSION/./}
   else
-    CONTAINER=php71
+    CONTAINER=php73
   fi
   docker exec -it -e SSH_AUTH_SOCK=/ssh-agent drupal_$CONTAINER bash -c "cd ${PWD/#$DOCKER_PATH_WEB/$docker_dest}; exec $args"
 }
